5ddcc7f2813b3c53a8dac5bd53f5b9ba0101de74
Chris Reynolds <chris@jazzsequence.com>
2025-02-18 09:27:35 -0700

    Default to draft PR (#15)
    * add the `--draft` flag
    
    * add a param to specify the pr status
    
    * define the other option (besides draft)
    
    * add --draft if 'open' was not passed
    
    * update readme to include available statuses
    
    * wrap variable in brackets
    
    * wrap PR_OPTIONS in quotes

--- bin/validate-plugin-version.sh
+++ bin/validate-plugin-version.sh
@@ -138,7 +138,11 @@ main() {
 	BASE_BRANCH="${BRANCH:-$DEFAULT_BRANCH}"
 
 	echo "Creating a pull request with base branch $BASE_BRANCH."
-	gh pr create --title "Update Tested Up To version to $CURRENT_WP_VERSION" --body "This pull request updates the \"Tested up to\" version in specified files (${FILENAMES}) to match the current WordPress version $CURRENT_WP_VERSION." --base "$BASE_BRANCH"
+	local PR_OPTIONS="--title \"Update Tested Up To version to $CURRENT_WP_VERSION\" --body \"This pull request updates the 'Tested up to' version in specified files (${FILENAMES}) to match the current WordPress version $CURRENT_WP_VERSION.\" --base \"$BASE_BRANCH\""
+	if [[ "${PR_STATUS:-}" != "open" ]]; then
+		PR_OPTIONS="${PR_OPTIONS} --draft"
+	fi
+	gh pr create "$PR_OPTIONS"
 }
 
 main
