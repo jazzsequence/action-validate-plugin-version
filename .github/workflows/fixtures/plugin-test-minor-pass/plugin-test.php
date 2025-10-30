12976f6f40ac2053405ff0a2488950a3cdbb9556
Chris Reynolds <chris@jazzsequence.com>
2025-10-30 15:46:59 -0600

    add more tests

---
+++ .github/workflows/fixtures/plugin-test-minor-pass/plugin-test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Test Plugin
+ * Plugin URI: https://github.com/jazzsequence/action-validate-plugin-version
+ * Description: A test plugin for the GitHub Action to validate plugin version
+ * Version: 1.0.0
+ * Author: Chris Reynolds
+ * Author URI: https://chrisreynolds.io
+ * License: MIT License
+ */
+
+// Silence is golden.
